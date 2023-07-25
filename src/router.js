7d875967f13330c0fe41232f61c07a825ecd3381
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-25 14:04:49 -0300

    - Aprendendo a definir rotas filhas usando vue-router  - Entendendo o funcionamento das classes adicionadas de router-link-active e router-link-exact-active e suas diferenças

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // import { createRouter, createWebHashHistory } from 'vue-router'
+import Contratos from '@/components/vendas/Contratos.vue'
 import Home from '@/views/Home.vue'
+import Leads from '@/components/vendas/Leads.vue'
 import Login from '@/views/Login.vue'
+import Servicos from '@/components/servicos/Servicos.vue'
 import Site from '@/views/Site.vue'
+import Vendas from '@/components/vendas/Vendas.vue'
 
 const routes = [
   {
@@ -11,7 +15,27 @@ const routes = [
   },
   {
     path: '/home',  //localhost:8080/home
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: 'vendas',
+        component: Vendas,
+        children: [
+          {
+            path: 'leads',
+            component: Leads
+          },
+          {
+            path: 'contratos',
+            component: Contratos
+          },
+        ]
+      },
+      {
+        path: 'servicos',
+        component: Servicos
+      }
+    ]
   }, 
   {
     path: '/login',
