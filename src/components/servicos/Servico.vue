1eb2f0a05aa44d04ad271da7526ed797f36c1df1
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-25 17:53:56 -0300

    - Reagindo a alterações na url com beforeRouteUpdate  - A implementação faz a mesma coisa que o watch que foi criado anteriormente

--- src/components/servicos/Servico.vue
+++ src/components/servicos/Servico.vue
@@ -16,13 +16,21 @@ export default {
   created() {
     this.getDadosApi(`http://localhost:3000/servicos/${this.$route.params.id}`);
   },
-  watch: { //Ouvintes
-    // Para identificar as mudanças de roteamento de serviço no momento em que o mesmo for atualizado
-    // Necessita ter um ouvinte programado e esse ouvinte é a propriedade $route (não confundir com $router)
-    // Essa propriedade $route é um método que será executado (quando houver qualquer mudança na propriedade $route que é inferida do vue-router)
-    $route(to) { //Convenção de parametro: to = novoValor, from = valorAntigo
-      if(to.params.id != undefined) this.getDadosApi(`http://localhost:3000/servicos/${to.params.id}`);
-    }
+  beforeRouteUpdate(to, from, next){ //Hook de route parecido com o método watch que foi criado anteriormente
+    //to = $route para onde estamos indo
+    //from = $route de onde estamos vindo
+    //next = fas com que o fluxo de navegação siga em frente
+
+    if(to.params.id != undefined) this.getDadosApi(`http://localhost:3000/servicos/${to.params.id}`);
+    next(); //next tem que vir no final para continuar a navegação
   }
+  // watch: { //Ouvintes
+  //   // Para identificar as mudanças de roteamento de serviço no momento em que o mesmo for atualizado
+  //   // Necessita ter um ouvinte programado e esse ouvinte é a propriedade $route (não confundir com $router)
+  //   // Essa propriedade $route é um método que será executado (quando houver qualquer mudança na propriedade $route que é inferida do vue-router)
+  //   $route(to) { //Convenção de parametro: to = novoValor, from = valorAntigo
+  //     if(to.params.id != undefined) this.getDadosApi(`http://localhost:3000/servicos/${to.params.id}`);
+  //   }
+  // }
 }
 </script>
