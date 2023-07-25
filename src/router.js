7ef3e2b10095007f316f79f1053c4e5069bedc3f
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-25 11:05:57 -0300

    - Aprendendo a definir router-links e entendendo o seu funcionamento

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 
