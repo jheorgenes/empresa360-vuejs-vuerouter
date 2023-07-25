0e04bab379f6af9173d1db17795034e41c05e601
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-25 10:55:50 -0300

    - Aprendendo a configurar o vue-router  - Definindo rotas com createWebHashHistory e createWebHistory

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+
+const routes = [
+  {
+    path: '/home',  //localhost:8080/home
+    component: Home
+  }, 
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes //Escrevendo de forma enchuta ao invés de routes: routes
+})
+
+export default router;
