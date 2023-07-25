a2fd428b1589543adca64733712bb8f3816a0fab
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-25 15:15:43 -0300

    - Aprendendo a manipular o Objeto de rota com this.$route

--- src/components/vendas/Lead.vue
+++ src/components/vendas/Lead.vue
@@ -1,3 +1,14 @@
 <template>
-  <h5>Detalhes do Lead</h5>
+  <h5>Detalhes do Lead {{ $route.params.id }}</h5>
 </template>
+
+<script>
+export default {
+  name: 'Lead',
+
+  // created() {
+  //   // console.log(this.$route); //Objeto da rota
+  //   console.log(this.$route.params);
+  // }
+}
+</script>
