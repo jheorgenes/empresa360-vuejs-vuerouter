9ae59b6ac6f456fdc4678e3ce2bcce308ce9934b
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-25 15:38:09 -0300

    - Implementando o recurso de visualização de um único lead

--- src/components/vendas/Lead.vue
+++ src/components/vendas/Lead.vue
@@ -1,11 +1,52 @@
 <template>
-  <h5>Detalhes do Lead {{ $route.params.id }}</h5>
+  <div>
+    <h5>{{ dados.id }} - {{ dados.nome }}</h5>
+    <br>
+    <div class="mb-3 row">
+      <label class="col-sm-2 col-form label">ID</label>
+      <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" :value="dados.id">
+      </div>
+    </div>
+    <div class="mb-3 row">
+      <label class="col-sm-2 col-form label">Nome</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" :value="dados.nome">
+      </div>
+    </div>
+    <div class="mb-3 row">
+      <label class="col-sm-2 col-form label">Telefone</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" :value="dados.telefone">
+      </div>
+    </div>
+    <div class="col-auto">
+      <button type="button" class="btn btn-primary">Atualizar</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Lead',
-
+  data: () => ({
+    dados: { 
+      id: Number, 
+      nome: String, 
+      telefone: String }
+  }),
+  methods: {
+    getDadosApi() {
+      fetch(`http://localhost:3000/leads/${this.$route.params.id}`)
+        .then(response => response.json())
+        .then(response => {
+          this.dados = response
+        })
+    }
+  },
+  created() {
+    this.getDadosApi();
+  },
   // created() {
   //   // console.log(this.$route); //Objeto da rota
   //   console.log(this.$route.params);
